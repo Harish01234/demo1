80650f1a2a6e80d6453ce599cdc72edb952310f4
Harish Malakar <harishmalakar19@gmail.com>
2024-07-02 12:32:10 +0530

    changed

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -32,11 +32,7 @@ function Page() {
       <div className='grid gap-4 m-9 sm:grid-cols-2 grid-cols-1'>
         <div>
           <h1 className='text-4xl text-gray-400'>Hisab Kitab</h1>
-          <h2 className='text-gray-400 mt-9'>
-            Hisab Kitab helps you keep track of money you&apos;ve lent and borrowed, including interest. 
-            You can also log extra transactions with notes. It automatically calculates your net balance 
-            and keeps all your financial records organized and easy to access.
-          </h2>
+          
         </div>
         <form onSubmit={signinhandler} className='grid gap-4 m-9'>
           <Input type='text' placeholder='Username' name='Username' onChange={(e) => setUser({ ...user, username: e.target.value })} />
